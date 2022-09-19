8d0879febed0f44a7f2fa5a0ff0f1513fe7b67a8
Ignacio Brenes Mendez <65673677+BrenesIgnacio@users.noreply.github.com>
2022-09-18 19:40:22 -0600

    Ejercicio 2

---
+++ Ejercicios Funcional haskell #2/Ejercicio 2.hs
@@ -0,0 +1,96 @@
+module Main where
+
+import Data.Ord (comparing)
+import Data.List (sortBy)
+
+--Autor @Ignacio Brenes
+--Ejercicios paradigma funcional haskel, Guia #2 de ejercicios Lenguajes de programacion
+
+--Ejercicio #2: 
+
+combine :: [a] -> [a] -> [[a]]
+combine p1 p2 = [[a,b] | (a, b) <- zip p1 p2]
+
+transformar :: [String] -> [(String, Integer)]
+transformar [] = []
+transformar (k:v:t) = (k, read v) : transformar t
+
+memb element lista =
+    or (map comp lista)
+    
+    where
+        comp x = (x == element)
+
+remove_if fun lista =
+    concat (map (\x -> if (fun x) then []
+                       else [x] )
+                lista)
+
+usar_vertices nodo grafo = do
+    let res =  concat (map (\x -> if (head (head x)) == nodo then [x]
+                            else [] ) 
+                            grafo)
+    if (res == []) then []
+    else head (tail (head res))
+
+extender ruta grafo =
+    remove_if (==[])
+    (map (\x -> if (memb x ruta) then []
+                       else x:ruta )
+        (orden_pesos (head ruta) grafo))
+
+prof ini fin grafo =
+    prof_aux fin [[ini]] grafo
+
+ord :: [[a]] -> [a]
+ord []=[]
+ord [y] = y
+ord (x:y:list)
+ |length x > length y = ord (y:list)            
+ |otherwise = ord (x:list)
+ 
+usar_pesos nodo grafo = do
+
+    let res =  concat (map (\x -> if (head(head x)) == nodo then [tail x]
+                            else [] ) 
+                            grafo)
+    if (res == []) then []
+    else head (tail (head res))
+
+prof_aux fin routes grafo =
+
+    if routes==[] then []
+    else if (fin == (head (head routes))) then
+        reverse (head routes):prof_aux fin ((extender (head routes) grafo)++(tail routes)) grafo
+    else
+        prof_aux fin ((extender (head routes) grafo)++(tail routes)) grafo
+    
+
+orden_pesos nodo grafo = do
+
+    let {combinar = combine (usar_vertices nodo grafo) (usar_pesos nodo grafo); concatenar = concat combinar; 
+
+    to_StringInt = transformar concatenar; ordenarConcat = sortBy (comparing (\(x,y) -> abs y)) to_StringInt; 
+
+    del_pesos = to_StringInt >>= \(x,y) -> [x]} in del_pesos
+
+-- Funcion Main
+
+main :: IO ()
+main = do
+
+    let grafo=[ [["i"],["a","b"], ["3","6"]],     
+                [["a"],["i","c","d"], ["3","6","5"]], 
+                [["b"],["i","c","d"], ["3","6","5"]],  
+                [["c"],["a","b","x"], ["3","6","5"]],  
+                [["d"],["a","b","f"], ["3","6","5"]], 
+                [["f"],["d"], ["3"]],          
+                [["x"],["c"], ["3"]]           
+                 ]   
+    
+    let lista=prof "i" "f" grafo
+
+    let new = ord lista
+
+    print("El camino mas corto encontrado es: ")
+    print(new)
